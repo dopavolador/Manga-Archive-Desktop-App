50d8b77fb8e01ee06d7f52122e4aec9d456fc0ab
Dopa Volador <77522030+Emiliarwr@users.noreply.github.com>
2023-11-03 10:32:10 -0300

    Add files via upload

--- MangaArchive/src/mangaarchive/vista/Main.java
+++ MangaArchive/src/mangaarchive/vista/Main.java
@@ -1,44 +1,65 @@
 
-
 package mangaarchive.vista;
 
-import javax.swing.*;
-import java.awt.*;
-
-public class Main extends JFrame {
-    
-    private JTabbedPane tabbedPane;
-    private JPanel panelAgregar, panelModificar, panelEliminar, panelListar;
+public class Main extends javax.swing.JFrame {
 
     public Main() {
-        setTitle("MANGA ARCHIVE");
-        setSize(800, 600);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        tabbedPane = new JTabbedPane();
-
-        panelAgregar = new JPanel();
-        panelAgregar.add(new JLabel("Agregar formulario aquí"));
-
-        panelModificar = new JPanel();
-        panelModificar.add(new JLabel("Modificar formulario aquí"));
-
-        panelEliminar = new JPanel();
-        panelEliminar.add(new JLabel("Eliminar formulario aquí"));
-
-        panelListar = new JPanel();
-        panelListar.add(new JLabel("Listado de productos aquí"));
-
-        tabbedPane.addTab("Agregar", panelAgregar);
-        tabbedPane.addTab("Modificar", panelModificar);
-        tabbedPane.addTab("Eliminar", panelEliminar);
-        tabbedPane.addTab("Listar", panelListar);
-
-        add(tabbedPane, BorderLayout.CENTER);
+        initComponents();
     }
-    
-    public static void main(String[] args) {
-        Main sistema = new Main();
-        sistema.setVisible(true);
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Main().setVisible(true);
+            }
+        });
     }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
 }
